50c7e78977e1f85af8e6da6663d88af3a7b9ed67
Daniel Nägele <git@danieln.de>
2022-10-13 09:07:48 +0200

    various updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,7 +2,7 @@ require('maps')
 require('options')
 
 if (vim.g.vscode) then
-  require("vscode")
+  vim.opt.loadplugins = false
 else
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -10,6 +10,5 @@ else
     packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
   end
-
   require('plugins')
 end
