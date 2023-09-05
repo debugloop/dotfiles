b22c9e9844822ca33296311aec6098ced5c34360
Daniel Nägele <d.naegele@gridx.de>
2023-09-05 08:26:32 +0200

    remove unused substituters

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -15,8 +15,6 @@
 
   nix.settings = {
     experimental-features = "nix-command flakes";
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
   nixpkgs = {
     hostPlatform = "x86_64-linux";
