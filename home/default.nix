111ac34e7089523f2bf08d96010a3583b604a45c
Daniel Nägele <d.naegele@gridx.de>
2024-01-25 17:28:01 +0100

    wezterm config

--- home/default.nix
+++ home/default.nix
@@ -183,11 +183,37 @@
         if wezterm.config_builder then
           config = wezterm.config_builder()
         end
+
+        -- under the hood
+        config.enable_wayland = true
+
+        -- fonts
         config.font = wezterm.font('FiraCode Nerd Font')
         config.font_size = 11.0
-        config.enable_wayland = false
-        config.color_scheme = 'kanagawabones'
+
+        -- visuals
         config.enable_tab_bar = false
+
+        -- color
+        config.force_reverse_video_cursor = true,
+        config.colors = {
+            foreground = "#dcd7ba",
+            background = "#1f1f28",
+
+            cursor_bg = "#c8c093",
+            cursor_fg = "#c8c093",
+            cursor_border = "#c8c093",
+
+            selection_fg = "#c8c093",
+            selection_bg = "#2d4f67",
+
+            scrollbar_thumb = "#16161d",
+            split = "#16161d",
+
+            ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+            brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+            indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+        },
         return config
       '';
     };
