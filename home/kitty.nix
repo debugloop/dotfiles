bf54473c09eebc32ff6bdc2750eef511d0d5d6a9
Daniel Nägele <d.naegele@gridx.de>
2024-05-13 15:49:57 +0200

    make nixfmt happy

--- home/kitty.nix
+++ home/kitty.nix
@@ -21,8 +21,7 @@
       scrollback_lines = "4000";
       scrollback_pager_history_size = "50";
       scrollback_fill_enlarged_window = "yes";
-      scrollback_pager = ''bash -c "exec nvim 63<&0 0</dev/null -c 'autocmd TermEnter * stopinsert' -c 'autocmd TermClose * call cursor(max([0,INPUT_LINE_NUMBER-1])+CURSOR_LINE, CURSOR_COLUMN)' -c 'terminal sed </dev/fd/63 -e \"s/'$'\x1b''\'']8;
-      file:[^\]*[\]//g\" && sleep 0.01 && printf \"'$'\x1b''\'']2;\"' -c 'set modifiable'"'';
+      scrollback_pager = ''bash -c "exec nvim 63<&0 0</dev/null -c 'autocmd TermEnter * stopinsert' -c 'autocmd TermClose * call cursor(max([0,INPUT_LINE_NUMBER-1])+CURSOR_LINE, CURSOR_COLUMN)' -c 'terminal sed </dev/fd/63 -e \"s/'$'\x1b''\'']8 file:[^\]*[\]//g\" && sleep 0.01 && printf \"'$'\x1b''\'']2;\"' -c 'set modifiable'"'';
 
       # selection
       strip_trailing_spaces = "smart";
