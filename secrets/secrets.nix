751bcf50a4e0665e13a4337cf43aeea1c46fa794
Daniel Nägele <d.naegele@gridx.de>
2024-06-23 11:20:19 +0200

    reencrypt age files

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,11 @@
 let
-  danieln = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXLvABfBx2ThhJ/nUYaLFu2QyLYomOn4BrKUnbwGeWk";
-  users = [ danieln ];
-
   simmons = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6PfN5CQE9VRocpilzDhhfaHfQwwC0mZkx4ndYTsS75";
   clarke = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbV3a87YcN9cxWyUeY6nAxpLGBxFJuAyC7Mh2iM6BJY";
   hyperion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5AsSvmh0/jPzl4gDynYuPnI4yFkK9srbAxPsQgL/sE";
-  systems = [ simmons clarke hyperion ];
+  all = [ simmons clarke hyperion ];
 in
 {
-  "password.age".publicKeys = users ++ systems;
-  "restic_rclone_config.age".publicKeys = users ++ systems;
-  "restic_password.age".publicKeys = users ++ systems;
+  "password.age".publicKeys = all;
+  "restic_rclone_config.age".publicKeys = all;
+  "restic_password.age".publicKeys = all;
 }
