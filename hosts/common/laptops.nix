0b5554097af5a63edaff7df7aa05bc1e645b54b9
Daniel Nägele <d.naegele@gridx.de>
2024-09-18 13:47:12 +0200

    make docker rootful

--- hosts/common/laptops.nix
+++ hosts/common/laptops.nix
@@ -14,10 +14,10 @@
   virtualisation = {
     docker = {
       enable = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
+      # rootless = {
+      #   enable = true;
+      #   setSocketVariable = true;
+      # };
     };
   };
 
