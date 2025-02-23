e5fd7009af13e58a376ff4632bd4efa21a1bdf94
Daniel Nägele <d.naegele@gridx.de>
2025-02-23 15:40:35 +0100

    remove dual nixpkgs conf

--- home/default.nix
+++ home/default.nix
@@ -13,11 +13,6 @@
     ./starship.nix
   ];
 
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowUnfreePredicate = _: true;
-  };
-
   home = {
     username = "danieln";
     homeDirectory = "/home/danieln";
