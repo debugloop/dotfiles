d52d238014de3c0e1f982173dfcd7b89387bb3a6
Daniel Nägele <git@danieln.de>
2025-04-12 16:20:51 +0200

    remove deprectaed opts

--- hosts/simmons/steam.nix
+++ hosts/simmons/steam.nix
@@ -3,13 +3,6 @@
   lib,
   ...
 }: {
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "steam"
-      "steam-original"
-      "steam-run"
-    ];
-
   programs.gamemode.enable = true;
   programs.steam = {
     enable = true;
