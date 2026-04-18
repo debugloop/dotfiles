435c61fc8176cdaa14fa468077399fa4080de3aa
Daniel Nägele <d.naegele@gridx.de>
2026-04-18 22:56:19 +0200

    feat: opencode instructions

--- modules/universal/ai.nix
+++ modules/universal/ai.nix
@@ -11,7 +11,21 @@ _: {
     ...
   }: {
     programs = {
-      opencode.enable = true;
+      opencode = {
+        enable = true;
+        context = ''
+          * When the conversation has multiple threads or topics, or if providing multiple alternative: Pick a
+            numbered identifier per item for the user to refer back to. If multiple sections need idengifiers, prefix
+            the number with a letter.
+          * Use the question tool to clarify whenever sensible, it avoids excessive numbered identifier reliance. E.g.
+            never ask inline whether the user is ready to proceed/implement, but rather use a question.
+          * Be exact in the way you follow instructions, do not jump into implementation necessarily even if you are in
+            build mode.
+          * If asked to implement something that does not appear to be idiomatic or optimal in some way, notify the user
+            about your doubts. The user is grateful for the opportunity to defend their decisions and improve their
+            judgement, especially if your objections are justified and well thought out.
+        '';
+      };
       claude-code.enable = true;
 
       # CLAUDE_CONFIG_DIR consolidates .claude.json into ~/.claude/ so the single
