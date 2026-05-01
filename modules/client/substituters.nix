4c236a5aba3273c74e6c97cfdc1b135c6dec140b
Daniel Nägele <d.naegele@gridx.de>
2026-05-01 07:50:51 +0200

    substituters: fix ssh key spec autogen

--- modules/client/substituters.nix
+++ modules/client/substituters.nix
@@ -3,30 +3,23 @@
   lib,
   ...
 }: let
-  # Get all nixosConfigurations that have both cache and tailscale enabled
-  # Tailscale is required for magic DNS to resolve hostnames
   cacheHosts =
     lib.filterAttrs (
       _: cfg: (cfg.config.nix.sshServe.enable or false) && (cfg.config.services.tailscale.enable or false)
     )
     inputs.self.nixosConfigurations;
 
-  # Read a host's SSH public key and encode it as base64 (URL-safe)
-  readHostKey = name: let
+  mkSubstituter = pkgs: name: _: let
     keyFile = inputs.self + "/keys/hosts/${name}.pub";
-    keyContent = builtins.readFile keyFile;
-    # Extract just the key part (not the comment)
-    keyParts = lib.splitString " " keyContent;
-    keyB64 = lib.elemAt keyParts 1;
-  in
-    keyB64;
-
-  # Generate a substituter entry for a cache host
-  mkSubstituter = name: _: let
-    keyB64 = readHostKey name;
-  in "ssh://nix-ssh@${name}?trusted=true&ssh-key=/etc/ssh/ssh_host_ed25519_key&priority=100&base64-ssh-public-host-key=${keyB64}&compress=true";
+    b64HostKey = builtins.readFile (pkgs.runCommandLocal "b64key-${name}" {} ''
+      ${pkgs.coreutils}/bin/head -1 ${keyFile} \
+        | ${pkgs.coreutils}/bin/cut -d' ' -f1,2 \
+        | ${pkgs.coreutils}/bin/base64 -w0 \
+        > $out
+    '');
+  in "ssh://nix-ssh@${name}?trusted=true&ssh-key=/etc/ssh/ssh_host_ed25519_key&priority=100&base64-ssh-public-host-key=${b64HostKey}&compress=true";
 in {
-  flake.modules.nixos.substituters = _: {
-    nix.settings.extra-substituters = lib.mapAttrsToList mkSubstituter cacheHosts;
+  flake.modules.nixos.substituters = {pkgs, ...}: {
+    nix.settings.extra-substituters = lib.mapAttrsToList (mkSubstituter pkgs) cacheHosts;
   };
 }
