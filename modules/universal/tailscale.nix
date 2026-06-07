95b772e2a959a4445aa875291ca21b9035e7df34
Daniel Nägele <d.naegele@gridx.de>
2026-06-07 09:29:20 +0200

    custom tailscale til nixpkgs update

--- modules/universal/tailscale.nix
+++ modules/universal/tailscale.nix
@@ -5,6 +5,21 @@ _: {
     inputs,
     ...
   }: {
+    # Pin to 1.96.4: versions 1.98.x have a bug where Config.Clone drops
+    # nil-valued Routes, breaking MagicDNS after any link change event.
+    # https://github.com/tailscale/tailscale/issues/19730
+    # Remove once a fixed release (> 1.98.1) is available in nixpkgs.
+    services.tailscale.package = pkgs.tailscale.overrideAttrs (_old: rec {
+      version = "1.96.4";
+      src = pkgs.fetchFromGitHub {
+        owner = "tailscale";
+        repo = "tailscale";
+        tag = "v${version}";
+        hash = "sha256-VnAEfY8W+2QPnQLvVFJA7/XyvSnppSdRvgAOgpmRFGM=";
+      };
+      vendorHash = "sha256-rhuWEEN+CtumVxOw6Dy/IRxWIrZ2x6RJb6ULYwXCQc4=";
+    });
+
     services.tailscale = {
       enable = true;
       authKeyFile = config.age.secrets.tailscaleAuthkey.path;
@@ -17,7 +32,7 @@ _: {
       useRoutingFeatures = "client";
     };
 
-    environment.systemPackages = [pkgs.tailscale];
+    environment.systemPackages = [config.services.tailscale.package];
 
     networking.firewall = {
       trustedInterfaces = [
