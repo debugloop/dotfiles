c43dd639f255e86277937a595d1bdfd348d110ca
Daniel Nägele <git@danieln.de>
2026-07-04 20:47:52 +0200

    add checks for hosts

--- modules/repo/flake.nix
+++ modules/repo/flake.nix
@@ -1,3 +1,8 @@
 {inputs, ...}: {
   imports = [inputs.flake-parts.flakeModules.modules];
+
+  flake.checks.x86_64-linux = {
+    roshar = inputs.self.nixosConfigurations.roshar.config.system.build.toplevel;
+    simmons = inputs.self.nixosConfigurations.simmons.config.system.build.toplevel;
+  };
 }
